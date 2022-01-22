5ccc986c2d6721a0ebbfd4b560dcbf285b86fdcf
Manas Mishra <durgdawaghar1998@gmail.com>
2022-01-22 20:08:11 +0530

    Incomplete

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ columns = [
     descriptionField,
 ]
 
+def populate_with_dummy_data(exports, exportFields):
+    pass
+
 if __name__ == "__main__":
     filepath = input("Enter psv file path: ")
 
@@ -20,15 +23,21 @@ if __name__ == "__main__":
     exports = []
     entryField = "Entry"
     partyField = "Party"
+    valueDateField = "Value Date"
     exportPsv = "export.psv"
+    exportFields = [
+        entryField,
+        partyField,
+        valueDateField,
+    ]
 
     with open(filepath, "r") as csvfile:
         for row in csv.DictReader(csvfile, delimiter='|'):
             row[descriptionField] = row[descriptionField].upper()
             if is_upi(row[descriptionField]):
                 upiSet.add(get_upi_username(row[descriptionField]))
-
-    update_upi_exports(upiSet, exports, entryField, partyField)
+    populate_with_dummy_data(exports, exportFields)
+    update_upi_exports(upiSet, exports, entryField, valueDateField, exportFields)
     with open(exportPsv, "w", newline="") as f:
         log.info("Exporting data to", exportPsv)
         writer = csv.DictWriter(f, fieldnames=dict(exports[0]).keys(), delimiter="|")
--- upi.py
+++ upi.py
@@ -9,9 +9,8 @@ def get_upi_username(description: str):
         log.exception("Invalid upi description")
     return upiSplits[3]
 
-def update_upi_exports(upiSet: set, exports: list, entryField: str, partyField):
+def update_upi_exports(upiSet: set, exports: list, entryField: str, valueDate: str, exportFields: list):
+    data = dict()
     for upiUsername in upiSet:
-        exports.append({
-            entryField: upiUsername,
-            partyField: "",
-        })
+        data[entryField] = upiUsername
+        exports.append(data)
