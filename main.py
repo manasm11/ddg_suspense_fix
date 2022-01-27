5c9bf2db1b05a57d333985084e23eae959b964f9
Manas Mishra <manas.m22@gmail.com>
2022-01-27 09:59:50 +0530

    Made the script to run for multiple psvs

--- main.py
+++ main.py
@@ -23,13 +23,15 @@ def get_dictionary(upiUser: str, dates: list, type):
         "Dates": ','.join(dates),
     }
 
-if __name__ == "__main__":
-    filepath = input("Enter psv file path: ")
+upiDates = defaultdict(list)
+exports = []
+exportPsv = "export.psv"
 
+def main(filepath):
+    global upiDates, exports, exportPsv
+    filepath = filepath.strip()
+    log.info(f"Processing {filepath}...")
     change_psv(filepath, columns)
-    upiDates = defaultdict(list)
-    exports = []
-    exportPsv = "export.psv"
 
     with open(filepath, "r") as csvfile:
         for row in csv.DictReader(csvfile, delimiter='|'):
@@ -38,6 +40,14 @@ if __name__ == "__main__":
                 upiDates[get_upi_username(row[descriptionField])].append(row[dateField])
     for upiUser in upiDates.keys():
         exports.append(get_dictionary(upiUser, upiDates[upiUser], type="UPI"))
+
+
+if __name__ == "__main__":
+    filepaths = input("Enter psv file path (file1,file2): ")
+
+    for filepath in filepaths.split(","):
+        main(filepath)
+
     with open(exportPsv, "w", newline="") as f:
         log.info("Exporting data to", exportPsv)
         writer = csv.DictWriter(f, fieldnames=dict(exports[0]).keys(), delimiter="|")
--- upi.py
+++ upi.py
@@ -1,10 +1,14 @@
 import log
 
 def is_upi(description: str):
-    return description.upper().startswith("UPI")
+    if description.upper().startswith("UPI"):
+        if len(description.split("/")) >= 4:
+            return True
+        log.error(f"startswith upi but not sufficient /s {description}")
+        return False
 
 def get_upi_username(description: str):
     upiSplits = description.split("/")
     if len(upiSplits) < 4:
-        log.exception("Invalid upi description")
+        log.exception(f"Invalid upi description: {description}. Skipping...")
     return upiSplits[3]
