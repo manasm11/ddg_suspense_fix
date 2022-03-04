24ffcba5f0de2c8c32130b99bc77158dbbcaa6bb
Manas Mishra <manas.m22@gmail.com>
2022-03-04 16:30:12 +0530

    Added beutiful cli option selection

--- __init__.py
+++ __init__.py
@@ -1,15 +1,38 @@
 """Entry point of the project."""
 
-import os
-
+import questions as q
 from parse_excels import generate_json_from_excels
 
 
 def main():
     """First function to be executed in the project."""
+    choices = {
+        "generate transactions.json from icici excels": generate_transactions_json,
+        "start frontend": start_frontend,
+        "update transactions.json from marg excels": update_transactions_json,
+    }
+    a = q.MAIN(choices.keys()).ask()
+    print(a)
+    choices[a]()
+
+
+def generate_transactions_json():
+    """Initiate generation of transactions json from icici excel files."""
+    answers = q.GENERATE_TRANSACTIONS.ask()
     generate_json_from_excels(
-        "input_excels", os.path.join("output_jsons", "transactions.json")
+        inExcelDirectory=answers["input_excels_directory"],
+        outJsonFile=answers["output_file"],
     )
+
+
+def start_frontend():
+    """Start frontend."""
+    pass
+
+
+def update_transactions_json():
+    """Update transactions json with the party details from marg excel files."""
+    pass
 
 
 if __name__ == "__main__":
---
+++ questions.py
@@ -0,0 +1,15 @@
+import questionary
+
+
+def MAIN(choices):
+    questionary.select("What to do ?", choices=choices)
+
+
+GENERATE_TRANSACTIONS = questionary.form(
+    output_file=questionary.path(
+        "Path of output file", default="output_jsons/transactions.json"
+    ),
+    input_excels_directory=questionary.path(
+        "Path of input icici excels directory", default="input_icici_excels"
+    ),
+)
