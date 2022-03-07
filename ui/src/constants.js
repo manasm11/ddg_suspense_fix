2254e6bf3adb52582f1a504a3830cfbfe102c446
Manas Mishra <manas.m22@gmail.com>
2022-03-07 18:06:44 +0530

    Changed password and tested with host as 0.0.0.0

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -1,6 +1,7 @@
 export default {
   SERVER_URL: "http://localhost:8000",
   SERVER_CHECK_INTERVAL_SECONDS: 5,
+  PASSWORD: "JAI MATA DI",
   MUST_HEADER:
     "g52l&cc^5l@@sREu(VY+i/Tpf@k7j3+lG9Y8ERUy0K+aE-Ikpyn`Cf8`SX6s&7aFf(MXFv/(%~=)`MYam*V-)(*qUkYnVWUPQBU@o^rAXW7YS04gK`4vnTxjwmRWc-1#",
   SINGLE_INPUT_CARD_DETAILS: {
--- ui/src/util.js
+++ ui/src/util.js
@@ -4,7 +4,7 @@ export default {
   sleep: (seconds) =>
     new Promise((resolve) => setTimeout(resolve, seconds * 1000)),
   is_res_ok: (res) => res.status == 200,
-  check_password: (password) => password === "HEALLO", // TODO: Improve logic to check password
+  check_password: (password) => password === c.PASSWORD,
   get_parties: async (query) =>
     query
       ? await fetch(join(c.SERVER_URL, "possible-parties") + `?desc=${query}`, {
